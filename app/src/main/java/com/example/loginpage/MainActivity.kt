a071422850a76f11839cb8fe3da06c4396a454c5
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-08-01 19:19:18 +0300

    second

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -9,21 +9,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var  binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnchat.setOnClickListener {
-           binding.btnchat.text="login"
-
-        }
+            binding.btnchat.text = "login"
 
 
-
-
-
-
-
-
-    }
-}
+        }    }     }
